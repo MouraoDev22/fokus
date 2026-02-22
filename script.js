7104173194bcb371dc16a383635548e4161f0851
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-22 16:45:44 -0300

    adicionado arquvio de aúdio

--- script.js
+++ script.js
@@ -6,9 +6,15 @@ const titulo = document.querySelector('.app__title');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+
 const temporizadorBtn = document.querySelector('.app__card-primary-button');
 
 const temporizador = document.querySelector('.app__card');
+
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
+musica.loop = true;
 
 let focoTempo = 1500;
 let curtoTempo = 300;
@@ -18,27 +24,33 @@ focoBtn.addEventListener('click', () => {
     alterarContexto('foco');
     
     focoBtn.classList.add('active');
-    curtoBtn.classList.remove('active');
-    longoBtn.classList.remove('active');
 });
 
 curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto');
     
     curtoBtn.classList.add('active');
-    longoBtn.classList.remove('active');
-    focoBtn.classList.remove('active');
 });
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo');
     
     longoBtn.classList.add('active');
-    curtoBtn.classList.remove('active');
-    focoBtn.classList.remove('active');
+});
+
+musicaFocoInput.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    } else {
+        musica.pause();
+    };
 });
 
 function alterarContexto(contexto) {
+    for (const botao of botoes) {
+        botao.classList.remove('active');
+    };
+    
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagens/${contexto}.png`);
 
