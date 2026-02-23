b789e5528a7213343b9ca05896595625619f90cb
Mourão <p3dr0sk0sk7160@gmail.com>
2026-02-23 00:07:30 -0300

    lógica de temporizador feita

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 const botoes = document.querySelectorAll('.app__card-button');
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 
-const temporizadorBtn = document.querySelector('.app__card-primary-button');
+const startPauseBtn = document.querySelector('#start-pause');
+const startPauseBtnSpan = document.querySelector('#start-pause span');
 
-const temporizador = document.querySelector('.app__card');
+const temporizador = document.querySelector('#timer');
 
 const musica = new Audio('/sons/luna-rise-part-one.mp3');
 musica.loop = true;
+
+const play = new Audio('/sons/play.wav');
+const pause = new Audio('/sons/pause.mp3');
+const beep = new Audio('/sons/beep.mp3');
+
+let idTemporizador = null;
 
 let focoTempo = 1500;
 let curtoTempo = 300;
@@ -24,18 +32,21 @@ focoBtn.addEventListener('click', () => {
     alterarContexto('foco');
     
     focoBtn.classList.add('active');
+    return;
 });
 
 curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto');
     
     curtoBtn.classList.add('active');
+    return;
 });
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo');
     
     longoBtn.classList.add('active');
+    return;
 });
 
 musicaFocoInput.addEventListener('change', () => {
@@ -44,6 +55,35 @@ musicaFocoInput.addEventListener('change', () => {
     } else {
         musica.pause();
     };
+    return;
+});
+
+startPauseBtn.addEventListener('click', () => {
+    if (idTemporizador) {
+        pause.play();
+        startPauseBtnSpan.textContent = 'Começar';
+        clearInterval(idTemporizador);
+        idTemporizador = null;
+        return;
+    };
+    
+    play.play();
+    startPauseBtnSpan.textContent = 'Pausar';
+
+    idTemporizador = setInterval(() => {
+        curtoTempo -= 1;
+        console.log(curtoTempo);
+        
+        if (curtoTempo === 250) {
+            beep.play();
+            startPauseBtnSpan.textContent = 'Começar';
+            curtoTempo = 300;
+            clearInterval(idTemporizador);
+            idTemporizador = null;
+            return;
+        };
+    }, 1000);
+    return;
 });
 
 function alterarContexto(contexto) {
@@ -82,4 +122,5 @@ function alterarContexto(contexto) {
         default:
             break;
     };
+    return;
 };
