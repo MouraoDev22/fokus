e2c43f04c86d74cffaaf32d5984949df749fb504
Mourão <p3dr0sk0sk7160@gmail.com>
2026-03-19 01:59:01 -0300

    feita função pura selecionarTarefa no ts

--- script-crud.ts
+++ script-crud.ts
@@ -7,3 +7,16 @@ interface EstadoAplicacao {
     tarefas: Tarefa[];
     tarefaSelecionada: Tarefa | null;
 };
+
+let estado: EstadoAplicacao = {
+    tarefas: JSON.parse(localStorage.getItem('tarefas') || '[]'),
+    tarefaSelecionada: null
+};
+
+function selecionarTarefa(estado: EstadoAplicacao, tarefa: Tarefa): EstadoAplicacao {
+
+    return {
+        ...estado,
+        tarefaSelecionada: tarefa === estado.tarefaSelecionada ? null : tarefa
+    };
+};
